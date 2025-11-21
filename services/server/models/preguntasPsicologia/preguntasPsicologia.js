c3c29e1a2d779582746f6cfa7ae569d1b14c9091
alejo Gonzalez <alejo.g31@hotmail.com>
2025-11-20 20:53:23 -0500

    Servicio de actualizacion de estado

--- services/server/models/preguntasPsicologia/preguntasPsicologia.js
+++ services/server/models/preguntasPsicologia/preguntasPsicologia.js
@@ -2,13 +2,14 @@ const { mongoose } = require('../../conection/mongo');
 
 const preguntaPsicologiaSchema = new mongoose.Schema(
     {
-        tipo: { type: String, required: true },                   
-        pregunta: { type: String, required: true },               
-        estado: { type: String, default: "activo" },             
-        id_usuario_creacion: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
+        tipo: { type: String, required: true },
+        pregunta: { type: String, required: true },
+        estado: { type: String, default: "activo" },
+        id_usuario_creacion: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        id_usuario_actualiza: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
     },
     {
-        timestamps: true,                                         
+        timestamps: true,
         collection: 'cl_preguntas_psicologia'
     }
 );
